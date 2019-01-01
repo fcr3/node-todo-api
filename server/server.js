489a9be712fcd604b0b4fde2f190e5b545fc40ce
Christian Reyes <fcreyes@berkeley.edu>
2019-01-01 13:24:48 -0800

    added login capability

--- server/server.js
+++ server/server.js
@@ -103,7 +103,17 @@ app.post('/users', (req, res) => {
   });
 });
 
-// Privatizing Routes
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
