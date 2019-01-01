a86feb4385de63d1014de19a8ec2644b250e8c50
Christian Reyes <fcreyes@berkeley.edu>
2019-01-01 10:19:35 -0800

    adding hashing for passwords

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,21 +1,32 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-/*
-jwt.sign
-jwt.verify
-*/
+// bcryptjs example below
+var password = '123abc!';
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var data = {
-  id: 10
-}
+var hashedPassword = '$2a$10$D5RmJxsMllQRpur4/aYefuWRRsw3il9/mG67Jzq5iAjHd6II5QCxq';
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// jsonwebtoken example below
+// var data = {
+//   id: 10
+// }
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded: ', decoded);
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded: ', decoded);
-
+// cryto-js example below
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 //
