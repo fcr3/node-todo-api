3d44a9296289781777ca7ccf1b8419cb7019d2b8
Christian Reyes <fcreyes@berkeley.edu>
2018-12-31 22:15:29 -0800

    made a config file and added support for test db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 
 module.exports = {mongoose};
